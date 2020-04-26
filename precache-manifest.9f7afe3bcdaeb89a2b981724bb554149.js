307961f40d3dabbcb9e8e4e40bac411e023ad631
tejas <tejas.ks.007@gmail.com>
2020-04-26 19:00:00 +0530

    Updates

---
+++ precache-manifest.9f7afe3bcdaeb89a2b981724bb554149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e669f0452de59f6dd61597110f8a56eb",
+    "url": "/movies-rolodex/index.html"
+  },
+  {
+    "revision": "2b06c100c0834172e58e",
+    "url": "/movies-rolodex/static/css/main.7c823cbd.chunk.css"
+  },
+  {
+    "revision": "c20d7772d9b71c998208",
+    "url": "/movies-rolodex/static/js/2.21ea3aa2.chunk.js"
+  },
+  {
+    "revision": "2b06c100c0834172e58e",
+    "url": "/movies-rolodex/static/js/main.3cd78a16.chunk.js"
+  },
+  {
+    "revision": "9114f30bb02194b00d31",
+    "url": "/movies-rolodex/static/js/runtime-main.2ba67336.js"
+  }
+]);
